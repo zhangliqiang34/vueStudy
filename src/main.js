e707c905d5bafd480205f100524b2790d9fabc48
zhangliqiang <wuyaping34@hotmail.com>
2019-11-12 21:16:38 +0800

    1112

--- src/main.js
+++ src/main.js
@@ -1,12 +1,22 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import Vuex from 'vuex';
 
 Vue.config.productionTip = false
 
 //全局过滤器方法
 Vue.filter("uppercase", value => {
     return value.toUpperCase();
+});
+
+//注意下面的代码
+Vue.use(Vuex);
+const store = new Vuex.Store({
+    state: {},
+    getters: {},
+    actions: {},
+    mutations: {}
 });
 new Vue({
     router,
