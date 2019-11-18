928fd10f1dd7f2f64271ca05e88d43685535a91c
zhangliqiang34 <wuyaping34@hotmail.com>
2019-11-18 17:28:38 +0800

    1118

---
+++ src/components/message.vue
@@ -0,0 +1,61 @@
+<template>
+    <div class="message" :style="styles">
+        <i class="iconfont" :class="icon"></i>
+        <div>{{message}}</div>
+    </div>
+</template>
+<script>
+export default {
+  props: ["type", "message"],
+  data() {
+    return {
+      types: {
+        error: {
+          icon: "icon-error",
+          color: "#f56c6c",
+          backgroundColor: "#fde2e2",
+          borderColor: "#fef0f0f0"
+        },
+        success: {
+          icon: "icon-success",
+          color: "#67c23a",
+          backgroundColor: "#f0f9eb",
+          borderColor: "#e1f3d8"
+        },
+        warn: {
+          icon: "icon-warn",
+          color: "#e6a23c",
+          backgroundColor: "#fdf6ec",
+          borderColor: "#faecd8"
+        },
+        info: {
+          icon: "icon-info",
+          color: "#909399",
+          backgroundColor: "#edf2fc",
+          borderColor: "#e4e7ed"
+        }
+      }
+    };
+  },
+  computed: {
+    icon() {
+      return this.types[this.type].icon;
+    },
+    styles() {
+      const styles = { ...this.types[this.type] };
+
+      delete styles.icon;
+      return styles;
+    }
+  }
+};
+</script>
+
+<style lang="less" scoped>
+@import "../assets/iconfont/iconfont.css";
+
+.message {
+  padding: 8px 16px;
+  line-height: 1.8;
+}
+</style>
